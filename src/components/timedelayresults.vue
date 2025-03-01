48e040cb494f9cf7588c6bf881bef554e7a349b0
sleuth79 <taymac@gmail.com>
2025-03-01 00:15:58 -0700

    please

---
+++ src/components/timedelayresults.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="time-delay-results">
+    <!-- ...existing code... -->
+    <p v-if="results.timeGap">
+      Time Gap:
+      <span class="result-value">{{ results.timeGap }}</span>
+    </p>
+    <!-- ...existing code... -->
+  </div>
+</template>
+<!-- ...existing script and style code... -->
